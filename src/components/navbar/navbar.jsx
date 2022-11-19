161f6ba960e769fa48209fcf57d0f3101c6e9a64
Tony Wu <tonywu@Tonys-Air-2.home>
2022-11-19 15:34:01 -0500

    changed icon

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,11 +5,11 @@ export default function NavComponent(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Hunter APAMSA</Navbar.Brand>
+        <Navbar.Brand href="/">Hunter APAMSA</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <Nav.Link href="https://us6.list-manage.com/survey?u=40df5102d206d3b304e227508&id=403969c2b5">Join Us</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
